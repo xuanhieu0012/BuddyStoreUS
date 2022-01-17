4380bbda3c454156efc17a238ef1d54c676f5eb1
Hieu <xuanhieu0012@gmail.com>
2022-01-16 21:44:16 -0600

    fixed bugs

--- client/src/components/homepage/ImageData.js
+++ client/src/components/homepage/ImageData.js
@@ -1,12 +1,12 @@
 import banner1 from '../../asset/banner1.jpg';
 import banner2 from '../../asset/banner2.jpg';
-import slide1 from '../../asset/slide1.jpg';
+import slide1 from '../../asset/Slide1.jpg';
 export const ImageData=[{
-    image: {banner1}
+    image: banner1
 },{
-    image: {banner2}
+    image: banner2
 },
 {
-    image: {slide1}
+    image: slide1
 }
 ]
