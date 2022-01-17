dd09213092fef5425165944caa206fafad59616e
Hieu <xuanhieu0012@gmail.com>
2022-01-16 21:39:13 -0600

    fixed minor errors

--- client/src/components/homepage/ImageData.js
+++ client/src/components/homepage/ImageData.js
@@ -1,12 +1,12 @@
+import banner1 from '../../asset/banner1.jpg';
+import banner2 from '../../asset/banner2.jpg';
+import slide1 from '../../asset/slide1.jpg';
 export const ImageData=[{
-    image: 'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.6435-9/188959810_367803015003076_4269822351090643000_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=e3f864&_nc_ohc=Gv08vzwAZKgAX-w1RvT&tn=_3xn2md1sh4satSI&_nc_ht=scontent-dfw5-2.xx&oh=39ebbfe6ae10c05a617d944ad8ed384c&oe=61AC5804'
+    image: {banner1}
 },{
-    image: 'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.6435-9/143855324_297321815384530_7778651152498063369_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=e3f864&_nc_ohc=gpMNscUSzP8AX-w9evM&tn=_3xn2md1sh4satSI&_nc_ht=scontent-dfw5-2.xx&oh=b00410de1af22b684332754fed2e709b&oe=61AD7CC9'
+    image: {banner2}
 },
 {
-    image: 'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.6435-9/158600325_319690899814288_696295942799975295_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=e3f864&_nc_ohc=H_HXUsoAIpUAX_a0GFW&tn=_3xn2md1sh4satSI&_nc_ht=scontent-dfw5-2.xx&oh=6c9c603b284a4f97da2cd65c594e5d56&oe=61AC9928'
-},
-{
-    image: 'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.6435-9/107501936_114136713703042_6688883979961441527_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=e3f864&_nc_ohc=XFxN2Ol_rj8AX_Yroi4&_nc_ht=scontent-dfw5-2.xx&oh=84adaa5ae5edc34a1807a70c8d3d6d3d&oe=61ABBAE3'
+    image: {slide1}
 }
 ]
